98761c80bc05b8f7078c75d75f6a0a5728c61582
彭慧 <penghui@dongpinbang.com>
2018-12-05 14:54:34 +0800

    update

--- learn_code/baseoperation.py
+++ learn_code/baseoperation.py
@@ -22,6 +22,35 @@ class Base(object):
         self.driver.quit()
 
 
+class Logger(object):
+
+    def __init__(self,log):
+        self.log=logging.getLogger(log)
+        self.log.setLevel(logging.DEBUG)
+        rq=time.strftime('%Y %m %d %H:%M:%S',time.localtime())
+        log_path=os.path.dirname(os.getcwd())+'/learn_code/Logs/'
+        log_name=log_path+rq+'.log'
+        fh=logging.FileHandler(log_name)
+        fh.setLevel(logging.INFO)
+
+        ch=logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+
+        formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        self.log.addHandler(fh)
+        self.log.addHandler(ch)
+
+    def getlog(self):
+        return self.log
+
+
+
+
+
 
 
 
--- learn_code/learn2.py
+++ learn_code/learn2.py
@@ -3,24 +3,32 @@
 
 from selenium import webdriver
 from baseoperation import Base
+from baseoperation import Logger
 
+
+mylog=Logger(log='my_test_log').getlog()
 
 class Test(object):
+
     driver=webdriver.Firefox(executable_path='/Users/ph/Documents/geckodriver')
     dr=Base(driver)
 
     def open_baidu(self):
         self.dr.open_url('https://baidu.com')
+        mylog.info('打开百度浏览器')
 
     def search(self,text):
-        self.driver.find_element_by_xpath('//*[@id="kw"]').send_keys('text')
+        self.driver.find_element_by_xpath('//*[@id="kw"]').send_keys(text)
+        mylog.info('输入框中输入文本')
         self.driver.find_element_by_xpath('//*[@id="su"]').click()
+        mylog.info('点击查找')
 
     def quit(self):
         self.dr.q()
+        mylog.info('退出浏览器')
 
 if __name__=='__main__':
     a = Test()
     a.open_baidu()
-    a.search('你好')
+    a.search(u'你好')
     a.quit()
