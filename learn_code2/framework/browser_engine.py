dfa113144060244b5b8005b32202c2f14b590d38
彭慧 <penghui@dongpinbang.com>
2018-12-05 16:16:52 +0800

    update

--- learn_code2/framework/browser_engine.py
+++ learn_code2/framework/browser_engine.py
@@ -1,6 +1,48 @@
+#coding=utf-8
 import ConfigParser
 import os.path
 from framework.logger import Logger
 from selenium import webdriver
 
+logger = Logger(log="BrowserEngine").getlog()
+
+
+class BrowserEngine(object):
+    dir = os.path.dirname(os.path.abspath('.'))  # 注意相对路径获取方法
+    chrome_driver_path = dir + '/tools/chromedriver'
+    firefox_driver_path = dir + '/tools/geckodriver'
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # read the browser type from config.ini file, return the driver
+    def open_browser(self, driver):
+        config = ConfigParser.ConfigParser()
+        # file_path = os.path.dirname(os.getcwd()) + '/config/config.ini'
+        file_path = os.path.dirname(os.path.abspath('.')) + '/config/config.ini'
+        config.read(file_path)
+        # config.read(file_path,encoding='UTF-8'), 如果代码有中文注释，用这个，不然报解码错误
+
+        browser = config.get("browserType", "browserName")
+        logger.info("You had select %s browser." % browser)
+        url = config.get("testServer", "URL")
+        logger.info("The test server url is: %s" % url)
+
+        if browser == "Firefox":
+            driver = webdriver.Firefox()
+            logger.info("Starting firefox browser.")
+        elif browser == "Chrome":
+            driver = webdriver.Chrome(self.chrome_driver_path)
+            logger.info("Starting Chrome browser.")
+
+        driver.get(url)
+        logger.info("Open url: %s" % url)
+        driver.implicitly_wait(10)
+        logger.info("Set implicitly wait 10 seconds.")
+        return driver
+
+    def quit_browser(self):
+        logger.info("Now, Close and quit the browser.")
+        self.driver.quit()
+        logger.info('browser.quit')
 
--- learn_code2/framework/logger.py
+++ learn_code2/framework/logger.py
@@ -1,21 +1,42 @@
+#coding=utf-8
 import logging
 import os.path
 import time
 
-class Log(object):
+
+class Logger(object):
 
     def __init__(self,log):
-        self.log=logging.getLogger(log)
-        self.log.setlevel(logging.DEBUG)
-
-        rq=time.strftime('%Y%m%d:%H%M%S'),time.localtime()
 
 
+        # 创建一个logger
+        self.log = logging.getLogger(log)
+        self.log.setLevel(logging.DEBUG)
 
+        # 创建一个handler，用于写入日志文件
+        rq = time.strftime('%Y.%m.%d-%H:%M', time.localtime())
+        # log_path = os.path.dirname(os.getcwd()) + '/Logs/'  # 项目根目录下/Logs 保存日志
+        log_path = os.path.dirname(os.getcwd()) + '/logs/'
+        # 如果case组织结构式 /testsuit/featuremodel/xxx.py ， 那么得到的相对路径的父路径就是项目根目录
+        log_name = log_path + rq + '.log'
+        fh = logging.FileHandler(log_name)
+        fh.setLevel(logging.INFO)
 
+        # 再创建一个handler，用于输出到控制台
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
 
+        # 定义handler的输出格式
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
 
+        # 给logger添加handler
+        self.log.addHandler(fh)
+        self.log.addHandler(ch)
 
+    def getlog(self):
+        return self.log
 
 
 
