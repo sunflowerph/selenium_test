b3ed1bec43c22397c5e127421655e7e41550e5bb
彭慧 <penghui@dongpinbang.com>
2018-12-06 14:37:34 +0800

    updata

---
+++ learn_code2/pageobjects/baidu_homepage.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from framework.base_page import Basepage
+
+
+class HomePage( Basepage):
+    input_box = "id=>kw"
+    search_submit_btn = "xpath=>//*[@id='su']"
+
+    def type_search(self, text):
+        self.type(self.input_box, text)
+
+    def send_submit_btn(self):
+        self.click(self.search_submit_btn)
+
+
--- learn_code2/testsuits/baidu_search.py
+++ learn_code2/testsuits/baidu_search.py
@@ -2,6 +2,7 @@
 import time
 import unittest
 from framework.browser_engine import BrowserEngine
+from pageobjects.baidu_homepage import HomePage
 
 
 class BaiduSearch(unittest.TestCase):
@@ -23,6 +24,22 @@ class BaiduSearch(unittest.TestCase):
         a.quit_browser()
 
     def test_baidu_search(self):
+        #使用POM方法，将定位和方法分别分装到了 pageobjects.baidu_homepage.py 和framework.base_page.py
+        homepage = HomePage(self.driver)
+        homepage.type_search('selenium')  # 调用页面对象中的方法
+        homepage.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
+        time.sleep(2)
+        homepage.get_window_img()  # 调用基类截图方法
+        try:
+            assert 'selenium' in homepage.get_page_title()  # 调用页面对象继承基类中的获取页面标题方法
+            print ('Test Pass.')
+        except Exception as e:
+            print ('Test Fail.', format(e))
+
+
+'''
+#
+    def test_baidu_search(self):
         """
         这里一定要test开头，把测试逻辑代码封装到一个test开头的方法里。
         :return:
@@ -35,6 +52,11 @@ class BaiduSearch(unittest.TestCase):
             print ('Test Pass.')
         except Exception as e:
             print ('Test Fail.', format(e))
+
+'''
+
+
+
 
 
 if __name__ == '__main__':
